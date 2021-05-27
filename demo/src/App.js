689093ef7411b890e0cf570dc00b24b25589b286
Charan <charanyan@gmail.com>
2021-05-27 15:59:02 -0500

    Update App.js

--- demo/src/App.js
+++ demo/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Github
         </a>
       </header>
     </div>
